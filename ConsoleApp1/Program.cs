30cb1cfbdf46984bc43ccf4294982cd5dc79912e
YuXiang Tong <60409201+team5-project@users.noreply.github.com>
2020-12-10 18:05:09 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"C:\Users\TTT\Desktop\123.xml");
+
+            XmlNode root = doc.SelectSingleNode("bookstore");
+            XmlNodeList childlist = root.ChildNodes;
+            string json = Newtonsoft.Json.JsonConvert.SerializeXmlNode(doc);
+
+            Console.WriteLine("XML -> JSON: {0}", json);
+
+            using (FileStream fs = new FileStream(@"C:\Users\TTT\Desktop\boolist.json", FileMode.OpenOrCreate, System.IO.FileAccess.ReadWrite, FileShare.ReadWrite))
+            {
+                using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    sw.WriteLine(json);
+                }
+            }
+        }
+    }
+  
+}
